1d9a3411eaab287454e0272ada7aca4d9eaf47a8
Jun <steve.jh.kang@gmail.com>
2019-09-21 23:43:58 +0900

    Update 43163.cpp

--- programmers/43163.cpp
+++ programmers/43163.cpp
@@ -21,10 +21,11 @@ bool is_right(string s1, string s2){
 int solution(string begin, string target, vector<string> words) {
     int answer = 0;
     queue<pair<string,int>> q;
-    q.push(make_pair(begin,0)); //시작을 넣어주고
+    q.push(make_pair(begin,0)); //BFS에서는 그냥 첫값 하나 넣어주기만 하면 됨.
     
     while(!q.empty()){
         string now=q.front().first; int cng=q.front().second; q.pop();
+        //처음에서 부터 끝까지 쭉 탐색하는데 조건 만족하는 것만 visit처리해주고 큐에 넣어주면 된다.
         for(int j=0;j<words.size();j++){
              //ooo 
             if(now==target){ //현재랑 target이랑 같으면 answer값 넣어주고 끝낸다.
