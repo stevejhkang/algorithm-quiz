3b83c8b4b701f4afb6097b43d3ffb25b88298fa4
Jun <steve.jh.kang@gmail.com>
2020-01-21 09:10:23 +0900

    Create SWEA_2050.java

---
+++ sw_expert/SWEA_2050.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class SWEA_2050 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+		ArrayList<Integer> arrayList = new ArrayList<>();
+//		scanner= new Scanner("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+		String string = scanner.next();
+		StringBuilder sBuilder = new StringBuilder();
+	
+		for(int i=0;i<string.length();i++) {
+			sBuilder.append(string.charAt(i)-'A'+1).append(" ");
+		}
+		System.out.println(sBuilder);		
+	}
+
+}
