0048c0f556d572a44f4d610ba05fe95b05b4d6b8
KimchiSoup(junu) <steve.jh.kang@gmail.com>
2020-01-30 08:51:31 +0900

    recursion

---
+++ project_folder/src/boj/BOJ_1074.java
@@ -0,0 +1,23 @@
+package boj;
+
+import java.util.Scanner;
+
+//Àç±Í
+public class BOJ_1074 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+		int n = scanner.nextInt();
+		int r = scanner.nextInt(); int c = scanner.nextInt();
+		
+	}
+	static int recursion(int n, int x, int y,int sum) {
+		if(n==0) {
+			sum+=x+2*y;
+			return sum;
+		}
+		
+		recursion()
+	}
+}
