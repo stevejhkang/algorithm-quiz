00a8aafeeb41c6fd67a71cc2b7fb4c7205733389
KimchiSoup(junu) <steve.jh.kang@gmail.com>
2020-02-04 08:52:47 +0900

    top

--- project_folder/src/jungol/JG_1809.java
+++ project_folder/src/jungol/JG_1809.java
@@ -7,25 +7,32 @@ public class JG_1809 {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		int n = scanner.nextInt();
-		
+		StringBuilder sb = new StringBuilder();
 		Stack<Integer> stack = new Stack<>();
-		for (int i = 0; i < n; i++) {
+		Stack<Integer> indexstack = new Stack<>();
+		
+		for(int i=1;i<=n;i++) {
 			int a= scanner.nextInt();
-			if(i==0) {
-				System.out.print(0+" ");
+			if(i==1) {
+				stack.push(a);
+				indexstack.push(1);
+				sb.append("0 ");
+				continue;
+			}
+			while(!stack.isEmpty()) {
+				if(a<stack.peek()) {
+					sb.append(indexstack.peek()+" ");
+					break;
+				}
+				stack.pop();
+				indexstack.pop();
 			}
 			if(stack.isEmpty()) {
-				stack.push(a);
+				sb.append("0 ");
 			}
-			else {
-				int top = stack.peek();
-				int cnt=0;
-				while(top<a) {
-					top=stack.pop();
-					cnt++;
-				}
-				System.out.println(c);
-			}
+			stack.push(a);
+			indexstack.push(i);
 		}
+		System.out.println(sb);
 	}
 }
