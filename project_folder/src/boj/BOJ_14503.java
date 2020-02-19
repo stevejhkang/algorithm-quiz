8a311e428ab4a442e0af473875a21015d781c53d
stevejhkang <stevejhkang@gmail.com>
2020-02-19 17:36:11 +0900

    keep going

---
+++ project_folder/src/boj/BOJ_14503.java
@@ -0,0 +1,13 @@
+package boj;
+/**
+ * @author steve.jh.kang@gmail.com
+ * @time 2020. 2. 19. 오후 4:08:10
+ * @category  
+ * @problem_description 로봇청소기가 주어졌을때 청소하는 영역의 개수를 구하는 프로그램
+ * @solving_description  
+ */
+public class BOJ_14503 {
+	public static void main(String[] args) {
+		
+	}
+}
